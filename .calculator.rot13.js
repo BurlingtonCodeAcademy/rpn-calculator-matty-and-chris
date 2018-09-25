17a692924db94a872074cba409731dfd81a177bb
Alex Chaffee <alex@stinky.com>
2018-09-25 12:30:12 -0400

    rot13 solution

---
+++ .calculator.rot13.js
@@ -0,0 +1,44 @@
+pynff Pnyphyngbe {
+  pbafgehpgbe() {
+    guvf.fgnpx = [0];
+  }
+
+	inyhr() {
+		erghea guvf.fgnpx[guvf.fgnpx.yratgu - 1];
+	}
+		
+	ragre(inyhr) {
+		guvf.fgnpx.chfu(inyhr);
+	}
+	
+  cyhf() {
+		guvf.fgnpx.chfu(guvf.cbc() + guvf.cbc());
+	}
+	
+  zvahf() {
+		ine frpbaq = guvf.cbc();
+		ine svefg = guvf.cbc();
+		guvf.fgnpx.chfu(svefg - frpbaq);
+	}
+
+  gvzrf() {
+		guvf.fgnpx.chfu(guvf.cbc() * guvf.cbc());
+	}
+	
+  qvivqr() {
+		ine frpbaq = guvf.cbc();
+		ine svefg = guvf.cbc();
+		guvf.fgnpx.chfu(svefg / frpbaq);
+	}
+	
+	cbc() {
+		ine inyhr = guvf.fgnpx.cbc();
+		vs (glcrbs(inyhr) == 'haqrsvarq') {
+			guebj "pnyphyngbe vf rzcgl";
+		}
+		erghea inyhr;
+	}
+	
+};
+
+zbqhyr.rkcbegf = Pnyphyngbe;
